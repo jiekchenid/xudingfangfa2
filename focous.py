65ba88fd4094a6cd91677ae7931f388b2aa8ebd3
jiek <jiekchenid@gmail.com>
2023-10-06 13:52:28 +0800

    Update focous.py

--- focous.py
+++ focous.py
@@ -1,11 +1,11 @@
 import time
-minutes = int (input("Enter the number of minutes to focus: "))
-seconds = minutes * 60
 
-while seconds:
-      mins, secs = divmod(seconds, 60)
-      timer = '{:02d}:{:02d}'.format(mins,secs)
-      print(timer,end="/r")
-      time.sleep(1)
-      seconds -= 1
- print("Time's up!")
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
